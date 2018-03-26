cff55f56e8dfec8c29fbd993f25e455c8639e58d
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2018-03-26 08:12:56 +0200

    Added utils script and matplotlib style file.

---
+++ utils/config.py
@@ -0,0 +1,66 @@
+from IPython.core.display import HTML, display
+from IPython import get_ipython
+
+################################################################################
+# Make notebooks look a bit better ...
+################################################################################
+
+display(HTML("""
+<style>
+div.output_area img, div.output_area svg {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 100%;
+    height: auto;
+}
+
+.rendered_html h1 {
+    font-size: 185.7%;
+    margin: 1.08em 0 0 0;
+    margin-top: 1.08em;
+    margin-bottom: 1.08em;
+    font-weight: bold;
+    line-height: 1.0;
+    text-align: center;
+}
+
+div.output_area img, div.output_area svg {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 100%;
+    height: auto;
+}
+
+div.cell {
+    margin-bottom: 30px;
+}
+
+#notebook-container {
+    padding: 60px;
+    background-color: #fff;
+    min-height: 0;
+    -webkit-box-shadow: 0px 0px 12px 1px rgba(87, 87, 87, 0.2);
+    box-shadow: 0px 0px 12px 1px rgba(87, 87, 87, 0.2);
+}
+</style>
+     """))
+
+################################################################################
+# Matplotlib Setup
+################################################################################
+
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+# Inline matplotlib
+ipython = get_ipython()
+ipython.magic("matplotlib inline")
+
+# Matplotlib style
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.rcParams["figure.figsize"] = (8,6)
+matplotlib.rcParams["font.size"] = 14
