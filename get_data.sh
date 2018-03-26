036b217817328f25be99cb285a472be4cdf3ed1e
simonpf <simonpf@chalmers.se>
2018-03-26 08:17:22 +0200

    Added missing cloud types files.

--- get_data.sh
+++ get_data.sh
@@ -2,9 +2,11 @@ mkdir -p data
 cd data
 wget http://spfrnd.de/data/ctp_qrnn/x_train.npy
 wget http://spfrnd.de/data/ctp_qrnn/y_train.npy
+wget http://spfrnd.de/data/ctp_qrnn/cloud_types_train.npy
 wget http://spfrnd.de/data/ctp_qrnn/x_val.npy
 wget http://spfrnd.de/data/ctp_qrnn/y_val.npy
 wget http://spfrnd.de/data/ctp_qrnn/x_test.npy
 wget http://spfrnd.de/data/ctp_qrnn/y_test.npy
+wget http://spfrnd.de/data/ctp_qrnn/cloud_types_test.npy
 cd ..
 mkdir -p models
